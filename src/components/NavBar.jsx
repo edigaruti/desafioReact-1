e6fa9fdd173042b1e233f84612a690393e01516d
edigaruti <edi.garuti@hotmail.com>
2022-03-29 15:49:40 -0300

    NavBar
    -Se creo el Nav Bar con sus elementos y links correspondientes
    -Se agregaron estilos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,57 @@ import React from "react";
 export default function NavBar() {
   return (
     <>
-      <div>hola soy navbar</div>
+      <div>
+        <nav>
+          <ul
+            style={{
+              listStyle: "none",
+              backgroundColor: "#509B4B",
+              width: "100%",
+              display: "inline-flex",
+              justifyContent: "center",
+              padding: "30px",
+            }}
+          >
+            <li style={{ paddingRight: "100px" }}>
+              <a
+                style={{
+                  color: "white",
+                  fontSize: "20px",
+                  textDecoration: "none",
+                }}
+                href="#"
+              >
+                Inicio
+              </a>
+            </li>
+            <li style={{ paddingRight: "100px" }}>
+              <a
+                style={{
+                  color: "white",
+                  fontSize: "20px",
+                  textDecoration: "none",
+                }}
+                href="#"
+              >
+                Tienda
+              </a>
+            </li>
+            <li style={{}}>
+              <a
+                style={{
+                  color: "white",
+                  fontSize: "20px",
+                  textDecoration: "none",
+                }}
+                href="#"
+              >
+                Nosotros
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 }
